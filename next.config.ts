27316f22085365edf4783f5e0891885b6fb5e646
Jeho05 <Jeho05@users.noreply.github.com>
2026-07-27 17:20:23 +0200

    Initial commit: Next.js project setup

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+/** @type {NextConfig} */
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3000/api/:path*",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
+
