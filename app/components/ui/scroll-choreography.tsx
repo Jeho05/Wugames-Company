3b7e768dc589dafbe5798f9b65145f1249c7d7ee
Jeho05 <Jeho05@users.noreply.github.com>
2026-07-29 19:44:04 +0200

    fix: reduced outer container to 110vh, removed pt causing empty bottom space

--- app/components/ui/scroll-choreography.tsx
+++ app/components/ui/scroll-choreography.tsx
@@ -57,8 +57,8 @@ export function ScrollChoreography({
     "absolute left-1/2 top-1/2 w-[36vw] h-[26vh] overflow-hidden -translate-x-1/2 -translate-y-1/2 bg-[#1e293b] shadow-2xl will-change-transform rounded-sm";
 
   return (
-    <div ref={containerRef} className={"relative h-[130vh] w-full pt-16 " + className}>
-      <div className="sticky top-16 h-[calc(100vh-64px)] w-full overflow-hidden">
+    <div ref={containerRef} className={"relative h-[110vh] w-full " + className}>
+      <div className="sticky top-20 h-[calc(100vh-80px)] w-full overflow-hidden">
         <div className="absolute inset-0 flex items-center justify-center">
           <motion.div
             style={{ x: tlX, y: tlY, opacity: underImagesOpacity }}
